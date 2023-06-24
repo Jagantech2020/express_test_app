51a2d246f8fce7424d9f82aed2ab29758beb18fb
Jaganraj <jagantech2020@gmail.com>
2023-06-24 22:19:54 +0530

    implemented basic navigation for the express app

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const { loggedInUser } = require("./main");
 
 const router = express.Router();
 
 router.get("/settings", (req, res, next) => {
+    console.log(loggedInUser.detail);
+    if(loggedInUser.detail && loggedInUser.detail.isLoggedIn){
     res.status(200).render("settings", {pageName: "Settings Page", pageTitle: "Settings"});
+    } else {
+        res.status(302).redirect("/");
+    }
 });
 
 router.get("/role", (req, res, next) => {
+    console.log(loggedInUser.detail);
+    if(loggedInUser.detail && loggedInUser.detail.isLoggedIn){
     res.status(200).render("role", {pageName: "Roles Page", pageTitle: "Role"});
+    } else {
+        res.status(302).redirect("/")
+    }
 })
 
 exports.adminRouter = router;
